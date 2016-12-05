1eef286ca423da9f36ae5837a9e08ace0d025149
Eemeli Heinonen <eemeli.heinonen@metropolia.fi>
2016-12-05 10:33:54 +0200

    Added a list of booleans to Discipline class, for checking if notifications have been sent

--- app/src/main/java/com/quattrofolia/balansiosmart/models/Discipline.java
+++ app/src/main/java/com/quattrofolia/balansiosmart/models/Discipline.java
@@ -1,15 +1,25 @@
 package com.quattrofolia.balansiosmart.models;
 
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.realm.Realm;
+import io.realm.RealmList;
 import io.realm.RealmObject;
 import io.realm.annotations.PrimaryKey;
 
-public class Discipline extends RealmObject implements AutoIncrementable {
+import static android.content.ContentValues.TAG;
+
+public  class Discipline extends RealmObject implements AutoIncrementable {
 
     @PrimaryKey
     private int id;
     private int frequency;
     private String monitoringPeriod;
+    private RealmList<RealmBoolean> list ;
+
 
     public int getFrequency() {
         return frequency;
@@ -17,6 +27,13 @@ public class Discipline extends RealmObject implements AutoIncrementable {
 
     public void setFrequency(int frequency) {
         this.frequency = frequency;
+        list = new RealmList<>();
+        for (int i = 0; i<frequency; i++) {
+            RealmBoolean b = new RealmBoolean();
+            b.setaBoolean(false);
+            this.list.add(b);
+        }
+        Log.d(TAG, "setFrequency: notificationChecklist size: "+list.size());
     }
 
     public MonitoringPeriod getMonitoringPeriod() {
