a8024c2a8d7960e1c1e330fe1d061ad37f99ecf2
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2016-12-11 15:31:38 +0200

    Removed debugging utilities, added completion fields

--- app/src/main/java/com/quattrofolia/balansiosmart/GoalItemRecyclerAdapter.java
+++ app/src/main/java/com/quattrofolia/balansiosmart/GoalItemRecyclerAdapter.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.quattrofolia.balansiosmart.models.Discipline;
@@ -28,16 +27,15 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
 
     private List<Goal> goals;
     private Instant now;
-    private Context parentContext;
 
     public static class GoalViewHolder extends RecyclerView.ViewHolder implements GoalItemClickListener {
 
         // Declare required views for goal items
         private View itemView;
         private CompletionRing completionRing;
+        private TextView textViewAccomplishments;
+        private TextView textViewFrequency;
         private TextView textViewType;
-        private TextView textViewDiscipline;
-        private LinearLayout scheduleView;
         private Context context;
 
         public GoalViewHolder(View v) {
@@ -45,9 +43,9 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
             itemView = v;
             context = v.getContext();
             completionRing = (CompletionRing) v.findViewById(R.id.goalItemCompletionCircle);
+            textViewAccomplishments = (TextView) v.findViewById(R.id.textView_accomplishments);
+            textViewFrequency = (TextView) v.findViewById(R.id.textView_disciplineFrequency);
             textViewType = (TextView) v.findViewById(R.id.textView_goalItemType);
-            textViewDiscipline = (TextView) v.findViewById(R.id.textView_discipline);
-            scheduleView = (LinearLayout) v.findViewById(R.id.layout_goalItemSchedule);
         }
 
         @Override
@@ -57,7 +55,6 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
 
             Intent intent = new Intent(itemView.getContext(), GoalDetailsActivity.class);
             intent.putExtra("GOAL_ID", goal.getId());
-            //intent.putExtra("userId", BalansioSmart.currentSession(realm).getUserId());
             context.startActivity(intent);
         }
     }
@@ -75,7 +72,6 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
 
     @Override
     public GoalItemRecyclerAdapter.GoalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        parentContext = parent.getContext();
         View inflatedView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.progress_view_goal_item_row, parent, false);
         return new GoalViewHolder(inflatedView);
@@ -88,7 +84,7 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
         Discipline discipline = goal.getDiscipline();
 
         if (discipline != null) {
-            float frequency = discipline.getFrequency();
+            int frequency = discipline.getFrequency();
             Interval currentPeriod = discipline.getMonitoringPeriod().quantizedInterval(now, 0);
             RealmResults<HealthDataEntry> entries;
             entries = Realm.getDefaultInstance()
@@ -99,37 +95,18 @@ public class GoalItemRecyclerAdapter extends RecyclerView.Adapter<GoalItemRecycl
                     .findAll();
             float completion;
             if (frequency > 0) {
-                completion = (float) entries.size() / frequency;
+                completion = (float) entries.size() / (float) frequency;
             } else {
                 completion = 0;
             }
 
             /* Update completion view */
             holder.completionRing.setCompletion(completion);
+            holder.textViewFrequency.setText("" + frequency);
+            holder.textViewAccomplishments.setText("" + entries.size());
 
             String goalTypeText = goal.getType().getLongName();
             holder.textViewType.setText(goalTypeText);
-
-            String disciplineText = "" + discipline.getFrequency() + " times a " + discipline.getMonitoringPeriod().toString();
-            holder.textViewDiscipline.setText(disciplineText);
-            TextView disciplineView = new TextView(holder.context);
-            disciplineView.setText(disciplineText);
-            disciplineView.setLayoutParams(new ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.WRAP_CONTENT,
-                    RecyclerView.LayoutParams.WRAP_CONTENT));
-            holder.scheduleView.addView(disciplineView);
-
-
-
-            List<Interval> intervals = goal.getDiscipline().getSchedule(now, 0);
-            for (Interval i : intervals) {
-                TextView cycle = new TextView(holder.context);
-                cycle.setText(i.toString());
-                cycle.setLayoutParams(new LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT));
-                holder.scheduleView.addView(cycle);
-            }
 
             holder.itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
