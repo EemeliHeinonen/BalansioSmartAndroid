ebdcb7d3e451f1ed8e21e9fc08056fe86242f848
Julius Niiniranta <julius.niiniranta@quattrofolia.com>
2016-12-11 15:34:24 +0200

    Added unified color resources

--- app/src/main/java/com/quattrofolia/balansiosmart/CompletionRing.java
+++ app/src/main/java/com/quattrofolia/balansiosmart/CompletionRing.java
@@ -2,7 +2,6 @@ package com.quattrofolia.balansiosmart;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.support.v4.content.ContextCompat;
@@ -14,7 +13,7 @@ import static android.graphics.Paint.Style.STROKE;
 
 public class CompletionRing extends View {
 
-    private static final String TAG = "CompletionCircle";
+    private static final String TAG = "CompletionRing";
     private Paint paint;
     private float width;
     private float strokeWidth;
@@ -27,7 +26,7 @@ public class CompletionRing extends View {
         paint = new Paint();
         paint.setAntiAlias(true);
         paint.setStyle(STROKE);
-        strokeWidth = 5;
+        strokeWidth = 6;
         paint.setStrokeWidth(strokeWidth);
         completion = 0;
         enabled = true;
@@ -38,29 +37,40 @@ public class CompletionRing extends View {
         super.onDraw(canvas);
         width = getWidth();
         radius = width / 2 - strokeWidth / 2;
-        float centerX = radius + (strokeWidth /2);
-        float centerY = radius + (strokeWidth /2);
+        float centerX = radius + (strokeWidth / 2);
+        float centerY = radius + (strokeWidth / 2);
 
         if (enabled) {
 
-            paint.setColor(ContextCompat.getColor(getContext(), R.color.testRed));
-            canvas.drawCircle(centerX, centerY, radius, paint);
+            if (completion >= 1) {
 
-            float arcRectStartingX = centerX - radius;
-            float arcRectStartingY = centerY - radius;
-            float arcRectEndingX = centerX + radius;
-            float arcRectEndingY = centerY + radius;
-            final RectF oval = new RectF();
-            oval.set(arcRectStartingX, arcRectStartingY, arcRectEndingX, arcRectEndingY);
+                // Discipline complete, draw a full green circle.
+                paint.setColor(ContextCompat.getColor(getContext(), R.color.bs_ok));
+                canvas.drawCircle(centerX, centerY, radius, paint);
 
-            float startAngle = 270;
-            float sweepAngle = completion * 360;
+            } else {
 
-            paint.setColor(ContextCompat.getColor(getContext(), R.color.testBlue));
-            canvas.drawArc(oval, startAngle, sweepAngle, false, paint);
-        } else {
-            paint.setColor(Color.GRAY);
-            canvas.drawCircle(centerX, centerY, radius, paint);
+                /* Discipline incomplete, draw a full circle with blank color and
+                * an arc with primary color to indicate the ratio of accomplishments
+                * to discipline frequency. */
+
+                paint.setColor(ContextCompat.getColor(getContext(), R.color.bs_blank));
+                canvas.drawCircle(centerX, centerY, radius, paint);
+
+                float arcRectStartingX = centerX - radius;
+                float arcRectStartingY = centerY - radius;
+                float arcRectEndingX = centerX + radius;
+                float arcRectEndingY = centerY + radius;
+                final RectF oval = new RectF();
+                oval.set(arcRectStartingX, arcRectStartingY, arcRectEndingX, arcRectEndingY);
+
+                float startAngle = 270;
+                float sweepAngle = completion * 360;
+
+                paint.setColor(ContextCompat.getColor(getContext(), R.color.bs_primary));
+                canvas.drawArc(oval, startAngle, sweepAngle, false, paint);
+
+            }
         }
     }
 
@@ -70,7 +80,7 @@ public class CompletionRing extends View {
     }
 
     public void disable() {
-        paint.setColor(Color.GRAY);
+        enabled = false;
         this.invalidate();
     }
 }
